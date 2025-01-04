02b98f66488f6fe250c2186e728a7d661d78cf93
Your Name <you@example.com>
2025-01-04 09:38:39 -0300

    criando  projeto meu-galo

---
+++ src/app/Componentes/CardGalo/cardGalo.jsx
@@ -0,0 +1,25 @@
+import { forwardRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import style from "../CardGalo/cardGalo.module.css";
+
+const CardGalo = (props) =>{
+    return(
+        <section className={style.containerGlobalCard}>
+            <div className={style.containerImagem} >
+               <Link href="*">
+                   <Image priority className={style.imagem} src={props.imagem} alt={props.alt} title={props.alt} />
+               </Link>
+            </div>
+            <div className={style.containerInformaçoesGalo}>
+                <h2>{props.nome}</h2>
+                <p>{props.raça}</p>
+                <p> Registro N° {props.numeroRegistro}</p>
+            </div>
+        </section>
+
+    )
+}
+
+export default CardGalo;
---
+++ src/app/ListaGalos/listaGalos.js
@@ -0,0 +1,23 @@
+import Galo1 from "../(public)/Galos/galo-indiogigante.jpg";
+import Galo2 from "../(public)/Galos/galoBranquelo.jpg";
+import Galo3 from "../(public)/Galos/galoCareca.jpg";
+import Galo4 from "../(public)/Galos/indioAlbino.jpg";
+import Galo5 from "../(public)/Galos/indioPintado.jpg"; 
+import Galo6 from "../(public)/Galos/indioMatador.jpg";
+
+
+ const Galos =[
+    {id:1,nome:"Gigante",imagem:Galo1, alt:"Galo Indio Gigante", raça:"Indio", numeroRegistro:"01"},
+    {id:2,nome:"Branquelo",imagem:Galo2, alt:"Galo Indio Bola", raça:"Indio",numeroRegistro:"02"},
+    {id:3,nome:"Careca",imagem:Galo3, alt:"Galo Indio Pescoço Pelado", raça:"Indio Mestiço",numeroRegistro:"03"},
+    {id:4,nome:"Albino",imagem:Galo4, alt:"Galo Indio Puro ",raça:"Indio",numeroRegistro:"04"},
+    {id:5,nome:"Pintado",imagem:Galo5, alt:"Galo Indio Pintado",raça:"Indio",numeroRegistro:"05"},
+    {id:5,nome:"matador",imagem:Galo6, alt:"Galo Indio Matador",raça:"Indio",numeroRegistro:"06"},
+    {id:1,nome:"Gigante",imagem:Galo1, alt:"Galo Indio Gigante", raça:"Indio", numeroRegistro:"01"},
+    {id:2,nome:"Branquelo",imagem:Galo2, alt:"Galo Indio Bola", raça:"Indio",numeroRegistro:"02"},
+    {id:3,nome:"Careca",imagem:Galo3, alt:"Galo Indio Pescoço Pelado", raça:"Indio Mestiço",numeroRegistro:"03"},
+    {id:4,nome:"Albino",imagem:Galo4, alt:"Galo Indio Puro ",raça:"Indio",numeroRegistro:"04"},
+    {id:5,nome:"Pintado",imagem:Galo5, alt:"Galo Indio Pintado",raça:"Indio",numeroRegistro:"05"},
+    {id:5,nome:"matador",imagem:Galo6, alt:"Galo Indio Matador",raça:"Indio",numeroRegistro:"06"}
+];
+export default Galos;
